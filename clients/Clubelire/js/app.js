ce24fb458ee2b81f4e5c5ba11c43699eae291587
akyag <akshay.226@gmail.com>
2016-03-26 19:51:30 +0530

    Clubelire - Home page changes

--- clients/Clubelire/js/app.js
+++ clients/Clubelire/js/app.js
@@ -1,1 +1,24 @@
 $(document).foundation();
+
+jQuery(document).ready(function(){
+  jQuery('#pictures-demo').slippry({
+  // general elements & wrapper
+  slippryWrapper: '<div class="sy-box pictures-slider" />', // wrapper to wrap everything, including pager
+
+  // options
+  adaptiveHeight: false, // height of the sliders adapts to current slide
+  //captions: true, // Position: overlay, below, custom, false
+  captionsSrc: 'title',
+  // pager
+  pager: false,
+
+  // controls
+  controls: false,
+  autoHover: false,
+
+  // transitions
+  transition: 'kenburns', // fade, horizontal, kenburns, false
+  kenZoom: 140,
+  speed: 3000 // time the transition takes (ms)
+  });
+});
